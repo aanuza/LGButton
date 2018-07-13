1b52346015c2950eae60126d294f516620c59817
Phitsanu Kaewtep <phitsanu.kaewtep@scb.co.th>
2018-07-14 00:06:18 +0700

    setup carthage support

---
+++ Example/LGButton/LGButton.h
@@ -0,0 +1,19 @@
+//
+//  LGButton.h
+//  LGButton
+//
+//  Created by Phitsanu Kaewtep on 7/13/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LGButton.
+FOUNDATION_EXPORT double LGButtonVersionNumber;
+
+//! Project version string for LGButton.
+FOUNDATION_EXPORT const unsigned char LGButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LGButton/PublicHeader.h>
+
+
